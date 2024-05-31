ddfce9aed6a842773ba5e48669cc5e99c680f9f4
M <malekuk9@gmail.com>
2024-05-31 22:20:34 +0100

    re group

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import uqlogo from "./assets/UQ_logo.svg";
+import "./App.css";
+import { SocialIcon } from "react-social-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="row centered">
+          <img src={uqlogo} alt="logo" className="App-logo" />
+          <div>
+            <div className="row spaced">
+              <SocialIcon url="https://t.me/+MXOdJ9ytlMoxNmFk" />
+              {/* <SocialIcon url="https://t.me/queerdom_bot" /> */}
+              {/* <SocialIcon url="https://instagram.com/unitedqueerdom" /> */}
+              {/* <SocialIcon url="https://www.linkedin.com/company/queerdom_uk" /> */}
+            </div>
+            <h4>Central Asia & Eastern Europe LGBTQ+ Community</h4>
+          </div>
+        </div>
       </header>
+      <body>
+        <h1 className="center">We're growing here 🪴</h1>
+      </body>
     </div>
   );
 }
